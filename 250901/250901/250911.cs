8fe2a0a36d6618a0482215d32b33f047efc60cb8
Nolinnomen <syoon1101@gmail.com>
2025-09-11 09:44:58 +0900

    250911
    .

---
+++ 250901/250901/250911.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250901
+{
+    internal class _250911
+    {
+        static void Main(string[] args)
+        {
+            void Swap(int a, int b)
+            {
+                Console.WriteLine($"a:{a}b:{b}");
+
+                int temp = a;
+                a = b;
+                b = temp;
+                Console.WriteLine("바꾼후");
+                Console.WriteLine($"a:{a}b:{b}");
+            }
+
+            Swap(3, 5);
+        }
+    }
+}
